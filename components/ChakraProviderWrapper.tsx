4201f810f1905fe58bae5b3f54894a878fa269ed
RayanEreteo <rayabf5@gmail.com>
2024-02-18 17:32:21 +0100

    Installation de chakra-ui

---
+++ components/ChakraProviderWrapper.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider } from "@chakra-ui/react"
+
+function ChakraProviderWrapper({children}: any) {
+  return (
+    <ChakraProvider>{children}</ChakraProvider>
+  )
+}
+
+export default ChakraProviderWrapper
