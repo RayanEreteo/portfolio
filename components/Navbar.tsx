6c3f131c61b15f87bb1c7e5d5dbb8d0b60809d80
RayanEreteo <rayabf5@gmail.com>
2024-02-20 16:17:00 +0100

    animation de la navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,25 @@
+"use client"
+
 import { Box, Button, Flex, Heading } from "@chakra-ui/react"
 import { MdEmail } from "react-icons/md"
+import { motion } from "framer-motion"
 
 function Navbar() {
+
+  const redirect = (redirectTo: string) => window.location.href = redirectTo;
+
   return (
     <nav>
         <Flex className="navbar" p={"5px"} h={"90px"} background={"#314E89"} color={"white"} alignItems={"center"} justifyContent={"space-between"}>
             <Heading>Rayan Ereteo</Heading>
-            <Flex w={"700px"} justifyContent={'space-between'} className="navbar-buttons-container">
-                <Button colorScheme="cyan">Qui suis-je ?</Button>
-                <Button colorScheme="cyan">Mes compétences</Button>
-                <Button colorScheme="cyan">Mes Projets</Button>
-                <Button colorScheme="green" w={"200px"} leftIcon={<MdEmail></MdEmail>}>Me Contacter</Button>
-            </Flex>
+            <motion.div animate={{x: -1100}}>
+              <Flex w={"700px"} position={"relative"} left={"1000px"} justifyContent={'space-between'} className="navbar-buttons-container">
+                <Button colorScheme="cyan" className="button">Qui suis-je ?</Button>
+                <Button colorScheme="cyan" className="button">Mes compétences</Button>
+                <Button colorScheme="cyan" className="button">Mes Projets</Button>
+                <Button colorScheme="green" className="button" w={"200px"} leftIcon={<MdEmail></MdEmail>}>Me Contacter</Button>
+              </Flex>
+            </motion.div>
         </Flex>
     </nav>
   )
