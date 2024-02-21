3132c3ce7b39bdbb0c1220e4aa92e0f953a042ed
RayanEreteo <rayabf5@gmail.com>
2024-02-21 15:07:31 +0100

    Ajout du text-section-container qui va être surement transformé en composant

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
+      <section className="presentation">
+        <Box className="text-section-container" m={"auto"} w={"fit-content"} mt={"6rem"}>
+          <Heading textAlign={'center'} fontSize={"3rem"}>Qui suis-je ?</Heading>
+          <Text>Mon nom est Rayan, je suis un jeune développeur de 21 ans spécialisé dans le développement web.</Text>
+        </Box>
+      </section>
     </main>
   );
 }
