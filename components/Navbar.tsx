4ed70a32535d0ab767f943d0888b741ec077eb5c
RayanEreteo <rayabf5@gmail.com>
2024-02-21 17:02:33 +0100

    ajout du margin au titre

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
   return (
     <nav>
         <Flex className="navbar" p={"5px"} h={"90px"} background={"#314E89"} color={"white"} alignItems={"center"} justifyContent={"space-between"}>
-            <Heading>Rayan Ereteo</Heading>
+            <Heading ml={"3rem"}>Rayan Ereteo</Heading>
             <motion.div animate={{x: -1100}}>
               <Flex w={"700px"} position={"relative"} left={"1000px"} justifyContent={'space-between'} className="navbar-buttons-container">
                 <Button colorScheme="cyan" className="button">Qui suis-je ?</Button>
