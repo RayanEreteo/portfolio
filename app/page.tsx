894fd7793baab92caa7f6083ae811f1fa9402380
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-01 21:57:14 +0100

    Update layout and styles for portfolio: add Orbitron font, update metadata, and apply new font class

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="font-orbitron">
       <main className="">
         <h1>Welcome to my portfolio</h1>
       </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        orbitron: ["var(--font-orbitron)", "sans-serif"],
+      },
     },
   },
   plugins: [],
