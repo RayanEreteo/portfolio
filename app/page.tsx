3ac274279411c7b135991da0f2bc1dd075e6f831
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-02 09:03:22 +0100

    Add Intro component to homepage and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Intro from "@/components/section/Intro";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-orbitron">
-      <main className="">
-        <h1>Welcome to my portfolio</h1>
+      <main>
+        <Intro />
       </main>
     </div>
   );
---
+++ components/section/Intro.tsx
@@ -0,0 +1,7 @@
+function Intro() {
+  return (
+    <div>Bio</div>
+  )
+}
+
+export default Intro
