26bb80588b86d9340c4f44253a379648f8940283
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-02 16:40:57 +0100

    Enhance StylizedButton with additional styling for improved appearance

--- components/StylizedButton.tsx
+++ components/StylizedButton.tsx
@@ -7,7 +7,7 @@ interface StylizedButtonProps {
 
 function StylizedButton({text, onClick}: StylizedButtonProps) {
   return (
-    <button onClick={onClick} className="stylized-button">{text}</button>
+    <button onClick={onClick} className="stylized-button bg-[#0D0E25] w-[100px] h-[50px] border-[1.2px] border-[#fff]">{text}</button>
   )
 }
 
