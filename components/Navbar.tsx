785cca370e9c220bf81c8c620e0b43d9cc74f973
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-03 13:47:31 +0100

    Enhance Navbar component with improved link styling and hover effects for better user experience

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import StylizedButton from './StylizedButton'
 
 function Navbar() {
-
     function downloadCV() {
         window.open('/CV_Développeur.pdf')
     }
@@ -14,11 +13,11 @@ function Navbar() {
             <div className="navbar-container flex flex-row items-center justify-around h-[5rem] text-[#FBE2E2] bg-[#002654] border-b-[1.2px] border-[#263d6c]">
                 <div className="name glow text-[30px]">Rayan Ereteo</div>
                 <div className="right-side inline-flex items-center">
-                    <div className="links flex justify-between w-[400px]">
-                        <a href="#about">A propos</a>
-                        <a href="#skills">Compétences</a>
-                        <a href="#projects">Projects</a>
-                        <a href="#contact">Contact</a>
+                    <div className="links flex justify-between w-[470px]">
+                        <a href="#about" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>A propos</a>
+                        <a href="#skills" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Compétences</a>
+                        <a href="#projects" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Projects</a>
+                        <a href="#contact" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Contact</a>
                     </div>
                     <div className="button-container ml-6">
                         <StylizedButton text='CV' onClick={downloadCV}/>
