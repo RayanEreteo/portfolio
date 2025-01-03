21798fab1d47677fab445ea23180f55e2a4b0286
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-03 15:34:11 +0100

    Enhance Intro component layout with image and presentation text

--- components/section/Intro.tsx
+++ components/section/Intro.tsx
@@ -1,8 +1,19 @@
+"use client"
+
+import Image from "next/image"
+
 function Intro() {
   return (
     <section>
-      <div className="flex justify-center items-center h-screen text-white">
-        
+      <div id="about" className="flex justify-around items-center h-screen text-white">
+        <div className="presentation-container">
+          <h1 className="text-5xl">Bonjour, je suis <span className="text-blue-500">Rayan</span></h1>
+          <p className="role text-2xl mt-3">Développeur web fullstack</p>
+          <p>Je suis un</p>
+        </div>
+        <div className="picture-container">
+          <Image src={"/moi.png"}  alt="moi" width={300} height={200}/>
+        </div>
       </div>
     </section>
   )
