a1f761126fc169f48d888cabc80b8bf0e3f3967a
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-14 16:28:04 +0100

    Update SoftSkillsCard component to adjust height and add margin for improved layout

--- components/SoftSkillsCard.tsx
+++ components/SoftSkillsCard.tsx
@@ -6,7 +6,7 @@ interface SoftSkillsCardProps {
 
 function SoftSkillsCard({title, desc}: SoftSkillsCardProps){
   return (
-    <div className="ss-card w-[400px] text-[#FFF] bg-[#0f327e] rounded border border-blue-400">
+    <div className="ss-card h-[122px] w-[400px] text-[#FFF] bg-[#0f327e] rounded border border-blue-400 mr-6">
         <p className="ss-title mb-6 font-semibold">{title}</p>
         <p className="ss-desc">{desc}</p>
     </div>
