37775b80149b9527d03aca8bfca45f7e8abe3eef
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-15 19:45:58 +0100

    Update Whyme component to add language SVGs and improve layout

--- components/section/Whyme.tsx
+++ components/section/Whyme.tsx
@@ -1,6 +1,9 @@
 import SoftSkillsCard from "../SoftSkillsCard"
+import Image from "next/image"
 
 function Whyme() {
+    const LANGUAGE_SVG_SIZE = 80
+
     return (
         <div id="skills" className="text-[#FBE2E2] flex flex-col mt-[5rem] h-[100vh]">
             <div className="soft-skills mb-6">
@@ -14,17 +17,21 @@ function Whyme() {
             <div className="technical-skills">
                 <h1 className="text-center text-3xl font-extrabold mb-5">Skills Technique : </h1>
                 <div className="text-center flex flex-row justify-center items-center">
-                    <SoftSkillsCard title="SEO" desc="Metadonnée, HTML semantic, Optimisation et bien plus pour ameliorer le score SEO de votre site web."/>
+                    <SoftSkillsCard title="SEO" desc="Métadonnée, HTML semantic, Optimisation et bien plus pour ameliorer le score SEO de votre site web."/>
                     <SoftSkillsCard title="Responsive" desc="Site web 100% responsive, adapté à tous les supports (mobile, tablette, desktop)."/>
                     <SoftSkillsCard title="Performance" desc="Optimisation des images, du code, du CSS et du JS pour un site web plus rapide."/>
                 </div>
             </div>
-            <div id="languages-framework">
-                <h1 className="text-center text-3xl font-extrabold mb-5 mt-5">Langages : </h1>
+            <div id="languages-framework" className="flex flex-row justify-around mt-[5rem]">
                 <div className="languages">
-                    
+                    <h1 className="text-center text-3xl font-extrabold mb-5">Langages :</h1>
+                    <Image src="/langframesvg/html5.svg" alt="html5" width={LANGUAGE_SVG_SIZE} height={LANGUAGE_SVG_SIZE} />
+                    <Image src="/langframesvg/css3.svg" alt="css3" width={LANGUAGE_SVG_SIZE} height={LANGUAGE_SVG_SIZE} />
+                    <Image src="/langframesvg/typescript.svg" alt="typescript" width={LANGUAGE_SVG_SIZE} height={LANGUAGE_SVG_SIZE} />
                 </div>
-                <div className="framework"></div>
+                <div className="framework">
+                    <h1 className="text-center text-3xl font-extrabold mb-5">Framework :</h1>
+                </div>
             </div>
         </div>
     )
