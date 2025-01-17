1072edd9914f9556bc65ea7e6e8a5e221ff61646
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-17 17:41:25 +0100

    Remove "A propos" link from Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,6 @@ function Navbar() {
                 <div className="name glow text-[30px]">Rayan Ereteo</div>
                 <div className="right-side inline-flex items-center">
                     <div className="links flex justify-between w-[470px]">
-                        <a href="#about" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>A propos</a>
                         <a href="#skills" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Compétences</a>
                         <a href="#projects" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Projects</a>
                         <a href="#contact" className='text-[1.2rem] hover:underline hover:scale-105 transition-all'>Contact</a>
