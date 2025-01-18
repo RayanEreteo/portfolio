4ae5d9ce1c9082c592808c1dfe75ca3ceca78544
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-01-18 15:44:20 +0100

    Enhance LangFrameworkContainer to support optional white SVG filter and add prop documentation

--- components/LangFrameworkContainer.tsx
+++ components/LangFrameworkContainer.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import Image from 'next/image'
+import { useState } from 'react'
 
 interface LangFrameworkContainerProps {
+    /** Size of the SVG */
     LANGUAGE_SVG_SIZE: number
+    /** Name of the SVG file (no .svg needed) */
     SVGName: string,
-    TechName: string
+    /** Name of the technology, the name will appear underneath the SVG */
+    TechName: string,
+    /** Should SVG be white ? */
+    whiteFilter?: boolean
 }
 
-function LangFrameworkContainer({LANGUAGE_SVG_SIZE, SVGName, TechName}: LangFrameworkContainerProps) {
+
+function LangFrameworkContainer({LANGUAGE_SVG_SIZE, SVGName, TechName, whiteFilter}: LangFrameworkContainerProps) {
+
   return (
     <div className="langframeworkcontainer">
-        <Image src={`/langframesvg/${SVGName}.svg`} alt="html5" width={LANGUAGE_SVG_SIZE} height={LANGUAGE_SVG_SIZE} />
+        <Image className={whiteFilter ? "white-svg mb-6" : "mb-6"} src={`/langframesvg/${SVGName}.svg`} alt="html5" width={LANGUAGE_SVG_SIZE} height={LANGUAGE_SVG_SIZE} />
         <h2 className='text-center font-semibold'>{TechName}</h2>
     </div>
   )
