0b9ade52ffd4f4352a8fe33100ed418ec4a19047
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-11 11:40:00 +0100

    Adjust Projects component height from 90vh to 60vh for improved layout

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -1,6 +1,6 @@
 function Projects() {
   return (
-    <div className="text-[#FBE2E2] flex flex-col mt-[5rem] h-[90vh]"></div>
+    <div className="text-[#FBE2E2] flex flex-col mt-[5rem] h-[60vh]"></div>
   )
 }
 
