87936465a7dfbb9f4603df50a76bc3b11934b29d
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-18 21:38:13 +0100

    Add Google reCAPTCHA to Contact form for spam protection

--- components/section/Contact.tsx
+++ components/section/Contact.tsx
@@ -1,10 +1,50 @@
+"use client"
+
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
 function Contact() {
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "";
+
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+  const [isVerified, setIsVerified] = useState(false);
+
+  async function handleCaptchaSubmission(token: string | null) {
+    try {
+      if (token) {
+        await fetch("/api", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ token }),
+        });
+        setIsVerified(true);
+      }
+    } catch (e) {
+      setIsVerified(false);
+    }
+  }
+
+  const handleChange = (token: string | null) => {
+    handleCaptchaSubmission(token);
+  };
+
+  function handleExpired() {
+    setIsVerified(false);
+  }
+
   return (
     <div id="contact-container" className="h-[80vh] flex justify-center items-center">
       <form action="" className="flex flex-col justify-between min-h-[250px]">
         <input className="p-3" type="email" placeholder="Votre email : " maxLength={50} required />
         <textarea className="p-3" name="" id="" placeholder="Votre message : " maxLength={200} rows={4} required></textarea>
-        <button type="submit" className="bg-red-600 p-3 text-white rounded">Envoyer</button>
+        <ReCAPTCHA sitekey={siteKey}
+        ref={recaptchaRef}
+        onChange={handleChange}
+        onExpired={handleExpired} />
+        <button disabled={!isVerified} type="submit" className="bg-red-600 p-3 text-white rounded">Envoyer</button>
       </form>
     </div>
   )
