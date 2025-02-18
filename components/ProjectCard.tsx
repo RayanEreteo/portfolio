d42136987255f1931db565a9be834723400e865e
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-18 20:17:55 +0100

    Refactor ProjectCard component to use specific thumbnail types and update link behavior based on development status

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,22 +3,23 @@ import ProjectCardArrow from "./ProjectCardArrow"
 interface projectI {
   projectName: string,
   projectDesc: string,
-  projectThumbnail: string,
+  projectThumbnail: "portfolio.png" | "extension.png"
   projectLink: string,
   inDev?: boolean
   moveLeft: () => void,
   moveRight: () => void
 }
 
-function ProjectCard({ projectName, projectDesc, projectLink, moveLeft, moveRight }: projectI) {
+function ProjectCard({ projectName, projectDesc, projectThumbnail, projectLink, moveLeft, moveRight, inDev }: projectI) {
   return (
     <div className={`project-card h-[100%] w-[100%] flex justify-around items-center`}>
+      <div className="thumbnail-container w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/thumbnail/${projectThumbnail})`}}></div>
       <ProjectCardArrow direction="left" onClick={moveLeft}/>
       <div className="text-content text-center">
         <h1 className="text-4xl font-bold">{projectName}</h1>
         <p className="text-2xl">{projectDesc}</p>
         <br />
-        <a className={`mt-11 bg-red-600 p-3 rounded`} href={projectLink} target="_blank">Visiter le projet</a>
+        <a className={`mt-11 ${inDev ? "bg-black pointer-events-none" : "bg-red-600"} p-3 rounded`} href={projectLink} target="_blank">{inDev ? "Projet en développement" : "Visiter le projet"}</a>
       </div>
       <ProjectCardArrow direction="right" onClick={moveRight}/>
     </div>
