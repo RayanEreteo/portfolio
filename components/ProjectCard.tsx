b73ea98776655ba663611c1cac86b3ead5756826
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-18 11:26:30 +0100

    Enhance ProjectCard component with navigation arrows and project details

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,28 @@
-function ProjectCard() {
+import ProjectCardArrow from "./ProjectCardArrow"
+
+interface projectI {
+  projectName: string,
+  projectDesc: string,
+  projectThumbnail: string,
+  projectLink: string,
+  inDev?: boolean
+  moveLeft: () => void,
+  moveRight: () => void
+}
+
+function ProjectCard({ projectName, projectDesc, projectLink, moveLeft, moveRight }: projectI) {
   return (
-    <div>ProjectCard</div>
+    <div className={`project-card h-[100%] w-[100%] flex justify-around items-center`}>
+      <ProjectCardArrow direction="left" onClick={moveLeft}/>
+      <div className="text-content text-center">
+        <h1 className="text-4xl font-bold">{projectName}</h1>
+        <p className="text-2xl">{projectDesc}</p>
+        <br />
+        <a className={`mt-11 bg-red-600 p-3 rounded`} href={projectLink} target="_blank">Visiter le projet</a>
+      </div>
+      <ProjectCardArrow direction="right" onClick={moveRight}/>
+    </div>
   )
 }
- 
+
 export default ProjectCard
