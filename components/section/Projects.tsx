54ca281ca6932e40967d5ed6f917f94ee9eaf899
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-18 11:26:21 +0100

    Created project component

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -1,6 +1,39 @@
+"use client"
+
+import ProjectCard from "../ProjectCard"
+import { useState } from "react";
+
 function Projects() {
+  const TRANSLATE_LIMIT = 3 - 1
+
+  const [translateCount, settranslateCount] = useState<number>(0)
+  const [pos, setPos] = useState<number>(0)
+
+  function moveLeft(){
+    if (translateCount == 0) return
+
+    setPos((prev) => prev -= 100)
+    settranslateCount((prev) => prev -= 1)
+
+    console.log(translateCount);
+  }
+
+  function moveRight(){
+    if (translateCount == TRANSLATE_LIMIT) return
+
+    setPos((prev) => prev += 100)
+    settranslateCount((prev) => prev += 1)
+
+    console.log(translateCount);
+    
+  }
+
   return (
-    <div className="text-[#FBE2E2] flex flex-col mt-[5rem] h-[60vh]"></div>
+    <div id="projects-container" className={`text-[#FBE2E2] w-[300vw] border-2 flex flex-row border-white mt-[5rem] mb-[5rem] h-[80vh] transition-all translate-x-[-${pos}vw]`}>
+      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"test"} projectName={"Portfolio"} projectLink="" projectDesc="Mon portfolio officiel. Retrouver mes projets et mes compétences."></ProjectCard>
+      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"test"} projectName={"Social Tree"} projectLink="" projectDesc="Créer une carte personnalisée ou vous pouvez afficher vos reseaux sociaux."></ProjectCard>
+      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"test"} projectName={"Extension anti-procrastination"} projectLink="" projectDesc="Une extension permettant de bloquer les sites web indiques."></ProjectCard>
+    </div>
   )
 }
 
