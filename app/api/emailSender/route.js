98a39824194fecaef2a2cbb4f6ac150ee652d3e2
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-19 20:33:26 +0100

    Add email sending functionality using nodemailer in API route

---
+++ app/api/emailSender/route.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer"
+import { NextResponse } from 'next/server';
+
+
+export async function POST(req, res) {
+    if (req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed' });
+
+    const body = await req.json();
+    const { email, message } = body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "rayabf5@gmail.com",
+            pass: process.env.GMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        to: "rayabf5@gmail.com",
+        subject: 'Message employeur',
+        text: message + `\n\nSend by : ${email}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+    return NextResponse.json({ message: 'Email sent' });
+}
