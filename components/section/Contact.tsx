ec796c488829aa98b07a18692e9c3e3cc09a2693
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-19 12:48:54 +0100

    Enhance contact form with additional styling and contact information section

--- components/section/Contact.tsx
+++ components/section/Contact.tsx
@@ -2,6 +2,8 @@
 
 import { useRef, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import { MdEmail, MdPhone } from "react-icons/md";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 function Contact() {
   const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "";
@@ -36,16 +38,34 @@ function Contact() {
   }
 
   return (
-    <div id="contact-container" className="h-[80vh] flex justify-center items-center">
-      <form action="" className="flex flex-col justify-between min-h-[350px]">
+    <div id="contact-container" className="h-[80vh] w-[100vw] flex justify-around items-center">
+      <form action="" className="flex flex-col justify-between min-h-[400px] bg-blue-900 p-5 border border-white rounded">
         <input className="p-3" type="email" placeholder="Votre email : " maxLength={50} required />
         <textarea className="p-3" name="" id="" placeholder="Votre message : " maxLength={200} rows={4} required></textarea>
         <ReCAPTCHA sitekey={siteKey}
         ref={recaptchaRef}
         onChange={handleChange}
         onExpired={handleExpired} />
-        <button disabled={!isVerified} type="submit" className="bg-red-600 p-3 text-white rounded">Envoyer</button>
+        <button disabled={!isVerified} type="submit" className="bg-red-600 p-3 text-white rounded disabled:bg-black disabled:cursor-not-allowed">Envoyer</button>
       </form>
+      <div className="contact-info-container text-white">
+        <div className="contact-info flex items-center mb-5">
+          <MdEmail size={50} color="white"/>
+          <p className="font-semibold ml-5">rayabf5@gmail.com</p>
+        </div>
+        <div className="contact-info flex items-center mb-5">
+          <MdPhone size={50} color="white"/>
+          <p className="font-semibold ml-5">07 44 54 35 56</p>
+        </div>
+        <div className="contact-info flex items-center mb-5">
+          <BsGithub size={50} color="white"/>
+          <p className="font-semibold ml-5">rayabf5@gmail.com</p>
+        </div>
+        <div className="contact-info flex items-center mb-5">
+          <BsLinkedin size={50} color="white"/>
+          <p className="font-semibold ml-5">07 44 54 35 56</p>
+        </div>
+      </div>
     </div>
   )
 }
