c9df4b6a263db695cdd9f285fa49139c64000de7
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-19 11:37:53 +0100

    Increase project display limit and add new weather application card

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "../ProjectCard"
 import { useState } from "react";
 
 function Projects() {
-  const TRANSLATE_LIMIT = 3 - 1
+  const TRANSLATE_LIMIT = 4 - 1
 
   const [translateCount, settranslateCount] = useState<number>(0)
   const [pos, setPos] = useState<number>(0)
@@ -29,10 +29,11 @@ function Projects() {
   }
 
   return (
-    <div id="projects-container" className={`text-[#FBE2E2] w-[300vw] border-2 flex flex-row border-white mt-[5rem] mb-[5rem] h-[80vh] transition-all`} style={{ transform: `translateX(-${pos}vw)` }}>
+    <div id="projects-container" className={`text-[#FBE2E2] w-[400vw] border-2 flex flex-row border-white mt-[5rem] mb-[5rem] h-[80vh] transition-all`} style={{ transform: `translateX(-${pos}vw)` }}>
       <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"portfolio.png"} projectName={"Portfolio"} projectLink="" projectDesc="Mon portfolio officiel. Retrouver mes projets et mes compétences."></ProjectCard>
       <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"extension.png"} projectName={"Extension anti-procrastination"} projectLink="https://github.com/RayanEreteo/AntiProcrastination" projectDesc="Une extension permettant de bloquer les sites web indiques."></ProjectCard>
       <ProjectCard inDev moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"socialtree.png"} projectName={"Social Tree"} projectLink="https://github.com/RayanEreteo/socialtree" projectDesc="Créer une carte personnalisée ou vous pouvez afficher vos reseaux sociaux."></ProjectCard>
+      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"meteo.png"} projectName={"Application Météo"} projectLink="https://infometeo-78814.web.app/" projectDesc="Une simple application utilisant l'API d'openweathermap ."></ProjectCard>
     </div>
   )
 }
