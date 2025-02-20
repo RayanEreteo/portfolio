30042064c55b2a429c31f3e9c54ab076fb599da6
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-20 13:22:44 +0100

    Update project links in Projects component for better accessibility

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -30,7 +30,7 @@ function Projects() {
 
   return (
     <div id="projects-container" className={`text-[#FBE2E2] w-[400vw] border-2 flex flex-row border-white mt-[5rem] mb-[5rem] h-[80vh] transition-all`} style={{ transform: `translateX(-${pos}vw)` }}>
-      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"portfolio.png"} projectName={"Portfolio"} projectLink="" projectDesc="Mon portfolio officiel. Retrouver mes projets et mes compétences."></ProjectCard>
+      <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"portfolio.png"} projectName={"Portfolio"} projectLink="https://github.com/RayanEreteo/portfolio" projectDesc="Mon portfolio officiel. Retrouver mes projets et mes compétences."></ProjectCard>
       <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"extension.png"} projectName={"Extension anti-procrastination"} projectLink="https://github.com/RayanEreteo/AntiProcrastination" projectDesc="Une extension permettant de bloquer les sites web indiques."></ProjectCard>
       <ProjectCard inDev moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"socialtree.png"} projectName={"Social Tree"} projectLink="https://github.com/RayanEreteo/socialtree" projectDesc="Créer une carte personnalisée ou vous pouvez afficher vos reseaux sociaux."></ProjectCard>
       <ProjectCard moveLeft={moveLeft} moveRight={moveRight} projectThumbnail={"meteo.png"} projectName={"Application Météo"} projectLink="https://infometeo-78814.web.app/" projectDesc="Une simple application utilisant l'API d'openweathermap ."></ProjectCard>
