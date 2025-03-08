708a4b5c567ca3f9857252be12ccdd9d57e35312
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-03-08 14:30:18 +0100

    Add cancelled state to ProjectCard and update usage in Projects component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -5,12 +5,13 @@ interface projectI {
   projectDesc: string,
   projectThumbnail: "portfolio.png" | "extension.png" | "socialtree.png" | "meteo.png"
   projectLink: string,
-  inDev?: boolean
+  inDev?: boolean,
+  cancelled?: boolean,
   moveLeft: () => void,
   moveRight: () => void
 }
 
-function ProjectCard({ projectName, projectDesc, projectThumbnail, projectLink, moveLeft, moveRight, inDev }: projectI) {
+function ProjectCard({ projectName, projectDesc, projectThumbnail, projectLink, moveLeft, moveRight, inDev, cancelled }: projectI) {
   return (
     <div className={`project-card h-[100%] w-[100%] flex justify-around items-center`}>
       <div className="thumbnail-container w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/thumbnail/${projectThumbnail})`}}></div>
@@ -19,7 +20,7 @@ function ProjectCard({ projectName, projectDesc, projectThumbnail, projectLink, 
         <h1 className="text-4xl font-bold">{projectName}</h1>
         <p className="text-2xl">{projectDesc}</p>
         <br />
-        <a className={`mt-11 ${inDev ? "bg-black pointer-events-none" : "bg-red-600"} p-3 rounded`} href={projectLink} target="_blank">{inDev ? "Projet en développement" : "Visiter le projet"}</a>
+        <a className={`mt-11 ${inDev || cancelled ? "bg-black pointer-events-none" : "bg-red-600"} p-3 rounded`} href={projectLink} target="_blank">{inDev || cancelled ? (inDev ? "Projet en développement" : "Projet annulé") : "Visiter le projet"}</a>
       </div>
       <ProjectCardArrow direction="right" onClick={moveRight}/>
     </div>
