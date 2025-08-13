2f3f1677bcd8c9c5a5b6f8fa8d1d36f77b9300eb
RayanEreteo <rayabf5@gmail.com>
2025-08-13 14:55:18 +0200

    Modification de la taille de la police dans le composant Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,10 @@ function Navbar() {
   return (
     <nav className="navbar font-franklin flex justify-center items-center h-[20vh] bg-cyan-900 text-white">
         <ul className="flex flex-row justify-around w-full">
-            <li><a className="font-extrabold text-5xl" href="">Introduction</a></li>
-            <li><a className="font-extrabold text-5xl" href="">Competences</a></li>
-            <li><a className="font-extrabold text-5xl" href="">Projets</a></li>
-            <li><a className="font-extrabold text-5xl" href="">Contact</a></li>
+            <li><a className="font-extrabold text-3xl" href="">Introduction</a></li>
+            <li><a className="font-extrabold text-3xl" href="">Competences</a></li>
+            <li><a className="font-extrabold text-3xl" href="">Projets</a></li>
+            <li><a className="font-extrabold text-3xl" href="">Contact</a></li>
         </ul>
     </nav>
   )
