16fe4efb7cefd61d999759db8386783810340ff1
RayanEreteo <rayabf5@gmail.com>
2025-08-14 19:54:05 +0200

    retrait texte gras

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,10 @@ function Navbar() {
   return (
     <nav style={{ backgroundColor: '#dadfe8' }} className="navbar font-franklin flex justify-center items-center h-[20vh] text-black">
         <ul className="flex flex-row justify-around w-full">
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-extrabold text-3xl" href="">Introduction</a></li>
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-extrabold text-3xl" href="">Competences</a></li>
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-extrabold text-3xl" href="">Projets</a></li>
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-extrabold text-3xl" href="">Contact</a></li>
+            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Introduction</a></li>
+            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Competences</a></li>
+            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Projets</a></li>
+            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Contact</a></li>
         </ul>
     </nav>
   )
