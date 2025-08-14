d4d98994aa8cf61c9d6d41f141edafc61083f0ae
RayanEreteo <rayabf5@gmail.com>
2025-08-14 20:48:27 +0200

    retrait section inutile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,6 @@ function Navbar() {
   return (
     <nav style={{ backgroundColor: '#dadfe8' }} className="navbar font-franklin flex justify-center items-center h-[20vh] text-black">
         <ul className="flex flex-row justify-around w-full">
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Introduction</a></li>
             <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Compétences</a></li>
             <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Projets</a></li>
             <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Contact</a></li>
