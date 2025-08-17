e32855a8a441953870920c45bfa27a1ffbea2f66
RayanEreteo <rayabf5@gmail.com>
2025-08-17 14:44:38 +0200

    Ajout de style

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -1,7 +1,8 @@
 function Introduction() {
   return (
-    <section>
-        <h1 className="text-4xl font-bold">Rayan Ereteo</h1>
+    <section id="introduction" className="mt-12 p-4">
+        <h1 className="text-7xl font-bold">Rayan Ereteo</h1>
+        <p>Un développeur passionné par la création d'applications web.</p>
         <p className="mt-4">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p>
     </section>
   )
