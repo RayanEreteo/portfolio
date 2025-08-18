22d4ec889951fdd9dd57728f6483c247f3441a56
RayanEreteo <rayabf5@gmail.com>
2025-08-18 12:10:10 +0200

    Retrait ID

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -2,7 +2,7 @@ function Introduction() {
   return (
     <section id="introduction" className="mt-12 p-4">
         <h1 className="text-7xl font-bold">Rayan Ereteo</h1>
-        <p id="typewriter-text" className="font-mono font-bold">Un développeur passionné par la création d'applications web.</p>
+        <p className="font-mono font-bold">Un développeur passionné par la création d'applications web.</p>
         <p className="mt-4">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p>
     </section>
   )
