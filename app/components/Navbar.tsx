bcae5e235a7909a0b0d087a7392e8c8cf9c8d436
RayanEreteo <rayabf5@gmail.com>
2025-08-18 14:03:19 +0200

    Ajout d'une animation d'entrée de la Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,30 @@
+"use client"
+import { motion } from "framer-motion";
+
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#dadfe8' }} className="font-franklin navbar flex justify-center items-center h-[20vh] text-black">
-        <ul className="flex flex-row justify-around w-full">
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Compétences</a></li>
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Projets</a></li>
-            <li className="flex flex-row group"><img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} /><a className=" font-light text-3xl" href="">Contact</a></li>
-        </ul>
-    </nav>
+    <motion.nav
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 60, damping: 12 }}
+      style={{ backgroundColor: '#dadfe8' }}
+      className="font-franklin navbar flex justify-center items-center h-[20vh] text-black"
+    >
+      <ul className="flex flex-row justify-around w-full">
+        <li className="flex flex-row group">
+          <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
+          <a className=" font-light text-3xl" href="">Compétences</a>
+        </li>
+        <li className="flex flex-row group">
+          <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
+          <a className=" font-light text-3xl" href="">Projets</a>
+        </li>
+        <li className="flex flex-row group">
+          <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
+          <a className=" font-light text-3xl" href="">Contact</a>
+        </li>
+      </ul>
+    </motion.nav>
   )
 }
 
