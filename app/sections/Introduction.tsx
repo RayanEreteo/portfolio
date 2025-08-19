c5b343f321a688e8638a01d4621ed3862a01dd58
RayanEreteo <rayabf5@gmail.com>
2025-08-19 20:07:06 +0200

    Changement text

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -4,7 +4,7 @@ function Introduction() {
   return (
     <section id="introduction" className="mt-12 m-auto text-center">
         <h1 className="text-7xl font-bold">Rayan Ereteo</h1>
-        <Typewriter textToWrite="Un développeur passionné par la création d'applications web." typeSpeed={50}/>
+        <Typewriter textToWrite="Un développeur passionné par l'informatique et la création d'applications." typeSpeed={30}/>
         {/* <p className="mt-6">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p> */}
     </section>
   )
