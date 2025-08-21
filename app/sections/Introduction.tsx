9b738b0d9ab140196e1de08d1969230d22dde3d9
RayanEreteo <rayabf5@gmail.com>
2025-08-21 22:47:46 +0200

    Texte d'introduction mobile responsive

--- app/sections/Introduction.tsx
+++ app/sections/Introduction.tsx
@@ -6,7 +6,7 @@ function Introduction() {
     <section id="introduction" className="mt-12 m-auto text-center">
         <h1 id="name-drop" className="text-7xl mb-4 p-2 font-bold">Rayan Ereteo</h1>
         <Typewriter textToWrite="Un développeur passionné par l'informatique et la création d'applications." typeSpeed={30}/>
-        <div className="introduction-wrapper w-[40rem] m-auto border-2 rounded border-black-50 bg-[#f0f0f0] p-4">
+        <div className="introduction-wrapper w-[50vw] lg:w-[40rem] m-auto border-2 rounded border-black-50 bg-[#f0f0f0] p-4">
           <p className="normal-text">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p>
         </div>
         <h3 className="medium-title">Qui suis-je ?</h3>
