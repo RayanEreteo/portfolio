da730ef9c8e609d3294d6e8b8d42b943277658ec
RayanEreteo <rayabf5@gmail.com>
2025-08-21 16:48:31 +0200

    Ajout du composant SoftSkillsCard

---
+++ app/components/Softskillscard.tsx
@@ -0,0 +1,15 @@
+type SoftskillsCardProps = {
+  title: string;
+  description: string;
+};
+
+function SoftskillsCard({ title, description }: SoftskillsCardProps) {
+  return (
+    <div className="softskills-card text-black bg-[#f0f0f0] p-4 border-2 rounded w-64 m-7 shadow-2xl">
+      <h3 className="softskills-card-title text-lg font-semibold">{title}</h3>
+      <p className="softskills-card-description text-sm">{description}</p>
+    </div>
+  )
+}
+
+export default SoftskillsCard
