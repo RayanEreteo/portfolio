bacaae218453957f10330ea676b7f34b44f0488b
RayanEreteo <rayabf5@gmail.com>
2025-08-29 17:50:19 +0200

    changement href

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
         <ul className="flex flex-row justify-around w-full">
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
-            <a className=" font-light text-3xl" href="/skills">Introduction</a>
+            <a className=" font-light text-3xl" href="/">Introduction</a>
           </li>
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
