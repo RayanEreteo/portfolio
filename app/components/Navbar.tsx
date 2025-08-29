39946c8b624ba21353c1909a4f01c12ea3c15d24
RayanEreteo <rayabf5@gmail.com>
2025-08-29 17:26:20 +0200

    ajout href navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,19 +14,19 @@ function Navbar() {
         <ul className="flex flex-row justify-around w-full">
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
-            <a className=" font-light text-3xl" href="">Introduction</a>
+            <a className=" font-light text-3xl" href="/skills">Introduction</a>
           </li>
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
-            <a className=" font-light text-3xl" href="">Compétences</a>
+            <a className=" font-light text-3xl" href="/skills">Compétences</a>
           </li>
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
-            <a className=" font-light text-3xl" href="">Projets</a>
+            <a className=" font-light text-3xl" href="/projects">Projets</a>
           </li>
           <li className="flex flex-row group">
             <img className="mr-5 opacity-0 group-hover:opacity-100 group-hover:scale-50 transition-all duration-300" src="diamond.svg" alt="diamond" width={30} />
-            <a className=" font-light text-3xl" href="">Contact</a>
+            <a className=" font-light text-3xl" href="/contact">Contact</a>
           </li>
         </ul>
       </motion.nav>
@@ -34,16 +34,16 @@ function Navbar() {
       <div  style={{ backgroundColor: '#dadfe8' }} className="mobile-navbar block md:hidden">
         <ul className="flex flex-col items-center w-full">
           <li className="mb-3">
-            <a className="font-light text-3xl" href="">Introduction</a>
+            <a className="font-light text-3xl" href="/">Introduction</a>
           </li>
           <li className="mb-3">
-            <a className="font-light text-3xl" href="">Compétences</a>
+            <a className="font-light text-3xl" href="/skills">Compétences</a>
           </li>
           <li className="mb-3">
-            <a className="font-light text-3xl" href="">Projets</a>
+            <a className="font-light text-3xl" href="/projects">Projets</a>
           </li>
           <li className="mb-3">
-            <a className="font-light text-3xl" href="">Contact</a>
+            <a className="font-light text-3xl" href="/contact">Contact</a>
           </li>
         </ul>
       </div>
