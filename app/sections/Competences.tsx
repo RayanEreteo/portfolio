b6bacc3296b6c055efa049599f074ead462e3fc9
RayanEreteo <rayabf5@gmail.com>
2025-08-29 19:27:45 +0200

    Ajout

--- app/sections/Competences.tsx
+++ app/sections/Competences.tsx
@@ -1,11 +1,16 @@
-"use client"
+import Image from "next/image"
 
 function Competences() {
-  return (
-    <div>
-        <img src="" alt=""/>
-    </div>
-  )
+    return (
+        <h1 className="medium-title">Mes Compétences</h1>
+    )
 }
+
+        // <Image
+        //     src="skillslogo/HTML.svg"
+        //     width={500}
+        //     height={500}
+        //     alt="Picture of the author"
+        // />
 
 export default Competences
