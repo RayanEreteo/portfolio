cb7dfeff46a9cd3f092da880b087e7491f8382f4
RayanEreteo <rayabf5@gmail.com>
2025-08-31 13:46:07 +0200

    Ajout composants skillsWrapper

---
+++ app/components/SkillsWrapper.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+
+type SkillsWrapperInfo = {
+    src: string,
+    alt: string,
+    techName: string,
+    w?: number
+}
+
+function SkillsWrapper({ src, alt, techName,w }: SkillsWrapperInfo) {
+    const imageSize: number = 70
+
+    return (
+        <div className="skill">
+            <Image
+                src={src}
+                width={w && w || imageSize}
+                height={imageSize}
+                alt={alt}
+            />
+            <p className="mt-5">{techName}</p>
+        </div>
+    )
+}
+
+export default SkillsWrapper
