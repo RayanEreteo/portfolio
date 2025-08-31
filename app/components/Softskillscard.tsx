22e45cb7b2e5cc5466efd12e83461a901be528b0
RayanEreteo <rayabf5@gmail.com>
2025-08-31 21:27:04 +0200

    retrait ombre

--- app/components/Softskillscard.tsx
+++ app/components/Softskillscard.tsx
@@ -5,7 +5,7 @@ type SoftskillsCardProps = {
 
 function SoftskillsCard({ title, description }: SoftskillsCardProps) {
   return (
-    <div className="softskills-card text-black bg-[#f0f0f0] p-4 border-2 rounded w-64 m-7 shadow-2xl">
+    <div className="softskills-card text-black bg-[#f0f0f0] p-4 border-2 rounded w-64 m-7">
       <h3 className="softskills-card-title lg:text-lg font-semibold">{title}</h3>
       <p className="softskills-card-description text-sm">{description}</p>
     </div>
