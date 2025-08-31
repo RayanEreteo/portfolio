62e2f4f5f059da08a07917349f3c09c9580df33b
RayanEreteo <rayabf5@gmail.com>
2025-08-31 21:27:26 +0200

    Ajout des placeholders tech skills

--- app/sections/Competences.tsx
+++ app/sections/Competences.tsx
@@ -1,28 +1,34 @@
 import SkillsWrapper from "../components/SkillsWrapper"
+import SoftskillsCard from "../components/Softskillscard"
 
 function Competences() {
     return (
-        <div className="skill-wrapper w-fit m-auto text-center">
+        <div className="skill-wrapper m-auto text-center flex flex-col justify-center items-center">
             <h1 className="large-title">Mes Compétences</h1>
-            <div className="languages-container-section bg-[#f0f0f0] w-[30vw] p-4 border-2 rounded m-7">
+            <div className="languages-container-section bg-[#f0f0f0] w-[30vw] p-4 border-2 rounded">
                 <p className="normal-text">Langages de programmation</p>
                 <div className="languages-container flex flex-row justify-around">
-                    <SkillsWrapper src="skillslogo/HTML.svg" techName="HTML" alt="HTML"/>
-                    <SkillsWrapper src="skillslogo/CSS.svg" techName="CSS" alt="CSS"/>
-                    <SkillsWrapper src="skillslogo/JS.svg" techName="JavaScript" alt="JS"/>
-                    <SkillsWrapper src="skillslogo/C.svg" techName="C" alt="C"/>
-                    <SkillsWrapper src="skillslogo/JAVA.svg" techName="Java" alt="JAVA"/>
+                    <SkillsWrapper src="skillslogo/HTML.svg" techName="HTML" alt="HTML" />
+                    <SkillsWrapper src="skillslogo/CSS.svg" techName="CSS" alt="CSS" />
+                    <SkillsWrapper src="skillslogo/JS.svg" techName="JavaScript" alt="JS" />
+                    <SkillsWrapper src="skillslogo/C.svg" techName="C" alt="C" />
+                    <SkillsWrapper src="skillslogo/JAVA.svg" techName="Java" alt="JAVA" />
                 </div>
             </div>
-            <div className="frameworks-container-section bg-[#f0f0f0] p-4 border-2 rounded m-7">
+            <div className="frameworks-container-section bg-[#f0f0f0] w-[30vw] p-4 border-2 rounded m-7">
                 <p className="normal-text">Frameworks / Librairie</p>
                 <div className="frameworks-container flex flex-row justify-around">
-                    <SkillsWrapper src="skillslogo/REACT.svg" techName="React" alt="REACT"/>
-                    <SkillsWrapper src="skillslogo/NEXT.svg" techName="NextJS" alt="NEXTJS"/>
-                    <SkillsWrapper src="skillslogo/NODE.svg" techName="NodeJS" alt="NODE"/>
-                    <SkillsWrapper src="skillslogo/EXPRESS.svg" techName="ExpressJS" alt="EXPRESS"/>
-                    <SkillsWrapper src="skillslogo/TAILWIND.svg" techName="Tailwind" alt="TAILWIND"/>
+                    <SkillsWrapper src="skillslogo/REACT.svg" techName="React" alt="REACT" />
+                    <SkillsWrapper src="skillslogo/NEXT.svg" techName="NextJS" alt="NEXTJS" />
+                    <SkillsWrapper src="skillslogo/NODE.svg" techName="NodeJS" alt="NODE" />
+                    <SkillsWrapper src="skillslogo/EXPRESS.svg" techName="ExpressJS" alt="EXPRESS" />
+                    <SkillsWrapper src="skillslogo/TAILWIND.svg" techName="Tailwind" alt="TAILWIND" />
                 </div>
+            </div>
+            <div className="tech-skills-section flex flex-row">
+                <SoftskillsCard title="Communication" description="Capacité à exprimer des idées clairement et à écouter les autres." />
+                <SoftskillsCard title="Passioné" description="Un intérêt profond pour la technologie et l'innovation." />
+                <SoftskillsCard title="Résolution de problèmes" description="Capacité à identifier des problèmes et à proposer des solutions." />
             </div>
         </div>
     )
