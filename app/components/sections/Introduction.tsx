a401c88104fc740b5a8730dea2a19ee7442882b4
RayanEreteo <rayabf5@gmail.com>
2025-09-02 12:48:59 +0200

    Inclus l'élément header

--- app/components/sections/Introduction.tsx
+++ app/components/sections/Introduction.tsx
@@ -4,11 +4,13 @@ import Typewriter from "../Typewriter"
 function Introduction() {
   return (
     <section id="introduction" className="mt-12 m-auto text-center">
-        <h1 id="name-drop" className="text-7xl mb-4 p-2 font-bold">Rayan Ereteo</h1>
-        <Typewriter textToWrite="Un développeur passionné par l'informatique et la création d'applications." typeSpeed={30}/>
-        <div className="introduction-wrapper w-[50vw] lg:w-[40rem] m-auto border-2 rounded border-black-50 bg-[#f0f0f0] p-4">
-          <p className="normal-text">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p>
-        </div>
+        <header>
+          <h1 id="name-drop" className="text-7xl mb-4 p-2 font-bold">Rayan Ereteo</h1>
+          <Typewriter textToWrite="Un développeur passionné par l'informatique et la création d'applications." typeSpeed={30}/>
+          <div className="introduction-wrapper w-[50vw] lg:w-[40rem] m-auto border-2 rounded border-black-50 bg-[#f0f0f0] p-4">
+            <p className="normal-text">Bienvenue sur mon portfolio où vous trouverez des informations sur mes compétences, mes projets et comment me contacter.</p>
+          </div>
+        </header>
         <h3 className="medium-title">Qui suis-je ?</h3>
         <div className="soft-skills-cards-container flex flex-col items-center sm:flex-row md:flex-row lg:flex-row lg:justify-around mt-[3rem]">
           <SoftskillsCard title="Communication" description="Capacité à exprimer des idées clairement et à écouter les autres." />
