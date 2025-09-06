c4d3b9cd560d956c69b2934e0b89cd45c531bbc3
RayanEreteo <rayabf5@gmail.com>
2025-09-06 14:14:02 +0200

    Ajout de l'importation de l'image "dot.svg" dans le composant Projets

--- app/components/sections/Projets.tsx
+++ app/components/sections/Projets.tsx
@@ -1,18 +1,24 @@
 import ProjectCard from "../ProjectCard"
+import Image from "next/image"
 
 function Projets() {
   return (
     <div className="skills-wrapper m-auto text-center">
-        <h1 className="large-title">Mes Projets</h1>
-        <p>
-          Vous trouverez ici une sélection de mes projets, mettant en avant mes compétences en développement web, front-end et back-end. <br />
-          Je vous invite à découvrir les codes sources disponibles sur mon <a className="underline text-blue-500" href="https://github.com/RayanEreteo" target="_blank" rel="noopener noreferrer">GitHub</a>.
-        </p>
-        <div className="projects-container grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
-          <ProjectCard title="Would You Rather" description="Un jeu de questions-réponses où les utilisateurs doivent choisir entre deux options." link="https://github.com/RayanEreteo/WouldYouRather" />
-          <ProjectCard title="Cute Cat" description="Un projet mettant en avant des chats mignons." link="https://github.com/RayanEreteo/CuteCat" />
-          <ProjectCard title="Extension anti-procrastination" description="Une extension de navigateur pour aider à lutter contre la procrastination." link="https://github.com/RayanEreteo/AntiProcrastination" />
+      <h1 className="large-title">Mes Projets</h1>
+      <p>
+        Vous trouverez ici une sélection de mes projets, mettant en avant mes compétences en développement web, front-end et back-end. <br />
+        Je vous invite à découvrir les codes sources disponibles sur mon <a className="underline text-blue-500" href="https://github.com/RayanEreteo" target="_blank" rel="noopener noreferrer">GitHub</a>.
+      </p>
+      <div className="projects-container grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+        <ProjectCard title="Would You Rather" description="Un jeu de questions-réponses où les utilisateurs doivent choisir entre deux options." link="https://github.com/RayanEreteo/WouldYouRather" />
+        <ProjectCard title="Cute Cat" description="Un projet mettant en avant des chats mignons." link="https://github.com/RayanEreteo/CuteCat" />
+        <ProjectCard title="Extension anti-procrastination" description="Une extension de navigateur pour aider à lutter contre la procrastination." link="https://github.com/RayanEreteo/AntiProcrastination" />
+        <div className="project-card flex items-center justify-between flex-col p-4 border border-gray-300 rounded-lg shadow-md m-4">
+          <h2 className="project-title text-3xl">{"Et plus encore..."}</h2>
+          <Image src="dot.svg" alt="Dot" width={100} height={100} />
+          <a className="underline text-blue-500" href="https://github.com/RayanEreteo" target="_blank" rel="noopener noreferrer">Accéder a mon github.</a>
         </div>
+      </div>
     </div>
   )
 }
