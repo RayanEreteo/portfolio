8b7d7c439aed9f8b467b49343d7c40e86cc363a2
RayanEreteo <rayabf5@gmail.com>
2025-09-09 21:22:52 +0200

    Ajout fonction form submit sans backend, et renom placeholder

--- app/components/sections/ContactCV.tsx
+++ app/components/sections/ContactCV.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 function ContactCV() {
     const [loading, setLoading] = useState<boolean>()
 
-    function onSubmit(e: React.FormEvent<HTMLFormElement>){
+    function sendEmail(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault();
         setLoading(true);
     }
@@ -17,8 +17,8 @@ function ContactCV() {
                 <a href="/CV_Développeur.pdf" download className="download-button bg-blue-500 text-white rounded-lg p-2 m-2 w-48 inline-block hover:bg-blue-600">Télécharger mon CV</a>
             </div>
             <div className="form-container mt-6 border-2 rounded border-black-50 bg-[#f0f0f0] w-[19rem] m-auto">
-                <form onSubmit={onSubmit} className="flex flex-col items-center font-bold placeholder:italic p-4">
-                    <input type="text" name="name" placeholder="Nom" required className="border border-gray-300 rounded-lg p-2 m-2 w-64" />
+                <form onSubmit={sendEmail} className="flex flex-col items-center font-bold placeholder:italic p-4">
+                    <input type="text" name="name" placeholder="Nom Complet" required className="border border-gray-300 rounded-lg p-2 m-2 w-64" />
                     <input type="email" name="email" placeholder="Email" required className="border border-gray-300 rounded-lg p-2 m-2 w-64" />
                     <textarea name="message" minLength={10} maxLength={150} placeholder="Message" required className="border border-gray-300 rounded-lg p-2 m-2 w-64 h-60 resize-none"></textarea>
                     <button type="submit" className="bg-blue-500 text-white rounded-lg p-2 m-2 hover:bg-blue-600 hover:cursor-pointer">{loading ? "Envoi en cours..." : "Envoyer"}</button>
