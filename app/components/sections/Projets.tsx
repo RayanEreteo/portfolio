80f930c5f65e902566fd72b34c1069198b54ac68
RayanEreteo <rayabf5@gmail.com>
2025-09-10 14:20:21 +0200

    Changement desc

--- app/components/sections/Projets.tsx
+++ app/components/sections/Projets.tsx
@@ -11,7 +11,7 @@ function Projets() {
       </p>
       <div className="projects-container grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
         <ProjectCard completed={false} title="Would You Rather" description="Un jeu de questions-réponses où les utilisateurs doivent choisir entre deux options." gitHubLink="https://github.com/RayanEreteo/WouldYouRather" />
-        <ProjectCard completed={true} title="Cute Cat" description="Un projet mettant en avant des chats mignons." projectLink="https://cutecat-two.vercel.app/" gitHubLink="https://github.com/RayanEreteo/CuteCat" />
+        <ProjectCard completed={true} title="Cute Cat" description="Un projet qui montre une photo de chat au hasard." projectLink="https://cutecat-two.vercel.app/" gitHubLink="https://github.com/RayanEreteo/CuteCat" />
         <ProjectCard completed={true} title="Extension anti-procrastination" description="Une extension de navigateur pour aider à lutter contre la procrastination." gitHubLink="https://github.com/RayanEreteo/AntiProcrastination" />
         <div className="project-card flex items-center justify-between flex-col p-4 border border-gray-300 rounded-lg shadow-md m-4">
           <h2 className="project-title text-3xl">{"Et plus encore..."}</h2>
