54368646443ca1adc66da2c599706a9b9a2f45d3
RayanEreteo <rayabf5@gmail.com>
2025-09-11 12:20:39 +0200

    modif. desc

--- app/api/mail/send/route.ts
+++ app/api/mail/send/route.ts
@@ -4,5 +4,5 @@ export async function POST(request: NextRequest) {
     const { name, email, message } = await request.json();
     if (!name || !email || !message) {return NextResponse.json({ success: false, message: "Merci de remplir tous les champs..." }, { status: 400 })}
 
-    return NextResponse.json({ success: true, message: "Email envoyée." }, {status: 200});
+    return NextResponse.json({ success: true, message: "Email envoyée avec succès." }, {status: 200});
 }
